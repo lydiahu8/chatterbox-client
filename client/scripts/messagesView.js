e797659e5451641110ea502f5022aa722d6e2930
lydiahu8 <lydiahu8@gmail.com>
2019-01-14 17:22:54 -0800

    Implement render method to append to MessagesView

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,15 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+
   },
 
   render: function() {
+    for (var key of Messages.messages) {
+      if (key.username !== undefined && key.text !== undefined && key.createdAt !== undefined) {
+        MessagesView.$chats.append(MessageView.render(key));
+      }
+    }
   }
 
 };
