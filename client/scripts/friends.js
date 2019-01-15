a2d641b092215ba2f5de950dd6e552ece5109b03
lydiahu8 <lydiahu8@gmail.com>
2019-01-14 21:43:24 -0800

    Implement toggleStatus method to add/remove friends

--- client/scripts/friends.js
+++ client/scripts/friends.js
@@ -1,4 +1,13 @@
 var Friends = {
+  storage: {},
 
-
+  toggleStatus: function(friend) {
+    console.log('in friends add');
+    if (this.storage[friend] === undefined) {
+      this.storage[friend] = 1;
+    } else {
+      delete this.storage[friend];
+    }
+  }
+  
 };
