44b6f2e2a48c423ec5dcc801d2d48fdf46c27602
lydiahu8 <lydiahu8@gmail.com>
2019-01-14 21:44:23 -0800

    Update to make username clickable

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -9,7 +9,16 @@ var MessagesView = {
   renderMessage: function(message) {
    
     if (message.username !== undefined && message.text !== undefined && message.roomname !== undefined) {
-      MessagesView.$chats.append(MessageView.render(message));
+      var htmlMessage = MessageView.render(message);
+
+      // console.log($(htmlMessage));
+
+      MessagesView.$chats.append(htmlMessage);
+
+      if (message.objectId !== undefined) {
+        $('.' + message.objectId).siblings('.username').on('click', function() { Friends.toggleStatus(message.username); });
+      }
+
     }
     
   }
