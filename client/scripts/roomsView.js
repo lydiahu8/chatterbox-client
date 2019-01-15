4715979a08adc2cc7d12249b57f441cb77e3157d
lydiahu8 <lydiahu8@gmail.com>
2019-01-14 19:32:10 -0800

    Implement handleAddRoom method

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -4,10 +4,18 @@ var RoomsView = {
   $select: $('#rooms select'),
 
   initialize: function() {
+    RoomsView.$button.on('click', RoomsView.handleAddRoom);
+    
   },
 
   renderRoom: function(roomname) {
     this.$select.append(new Option(roomname, roomname));
+  },
+
+  handleAddRoom: function() {
+    Rooms.add($('#addroom').val());
+    RoomsView.renderRoom($('#addroom').val());
+    $('#addroom').val('');
   }
 
 };
