7219087d1917e9214c3d5fda3bdf0586ef7ec69f
lydiahu8 <lydiahu8@gmail.com>
2019-01-14 17:18:58 -0800

    Implement fetch method to save data to messages.js

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,23 +7,38 @@ var App = {
   initialize: function() {
     App.username = window.location.search.substr(10);
 
+    MessagesView.initialize();
     FormView.initialize();
     RoomsView.initialize();
-    MessagesView.initialize();
 
     // Fetch initial batch of messages
     App.startSpinner();
     App.fetch(App.stopSpinner);
+   
 
   },
 
   fetch: function(callback = ()=>{}) {
     Parse.readAll((data) => {
       // examine the response from the server request:
-      console.log(data);
+
+      for (var key of data.results) {
+        Messages.messages.push(key);
+        if (key.username !== undefined && key.text !== undefined && key.createdAt !== undefined) {
+          key.username = this.escapeData(key.username);
+          key.text = this.escapeData(key.text);
+          key.createdAt = this.escapeData(key.createdAt);
+        }
+      }
+
+      MessagesView.render();
 
       callback();
     });
+  },
+
+  escapeData: function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\\<\>]/g, '\\$&');
   },
 
   startSpinner: function() {
