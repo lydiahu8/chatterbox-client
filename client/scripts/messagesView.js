70b3fe162e24846b949c75392a5916a7ab585c13
lydiahu8 <lydiahu8@gmail.com>
2019-01-15 19:50:30 -0800

    Remove functionality to initially display all chats

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -7,38 +7,38 @@ var MessagesView = {
   },
 
   renderMessage: function(message) {
-    if (RoomsView.getSelectedRoom() === '---DEFAULT---' || (message.roomname !== undefined && message.roomname === RoomsView.getSelectedRoom())) {
-      if (message.username !== undefined && message.text !== undefined) {
-        Messages.messages.push(message);
+    // if (RoomsView.getSelectedRoom() === '---DEFAULT---' || (message.roomname !== undefined && message.roomname === RoomsView.getSelectedRoom())) {
+    if (message.username !== undefined && message.text !== undefined) {
+      Messages.messages.push(message);
 
-        if (message.roomname === undefined) {
-          message.roomname = '';
-        }
-
-        if (message.objectId === undefined && message.objectId !== '') {
-          message.objectId = 'dummy' + (Messages.messages.length - 1);
-        }
-
-        if (message.createdAt === undefined) {
-          message.createdAt = '';
-        }
-
-        
-        var htmlMessage = MessageView.render(message);
-
-        MessagesView.$chats.append(htmlMessage);
-
-        if (Friends.storage[message.username] !== undefined) {
-          $('.' + message.objectId)[0].parentElement.className = 'friend';
-        }
-
-        $('.' + message.objectId).siblings('.username').on('click', function() { 
-          Friends.toggleStatus(message.username); 
-          $('#message').val('');
-          App.fetch(App.stopSpinner);
-        });
+      if (message.roomname === undefined) {
+        message.roomname = '';
       }
+
+      if (message.objectId === undefined && message.objectId !== '') {
+        message.objectId = 'dummy' + (Messages.messages.length - 1);
+      }
+
+      if (message.createdAt === undefined) {
+        message.createdAt = '';
+      }
+
+      
+      var htmlMessage = MessageView.render(message);
+
+      MessagesView.$chats.append(htmlMessage);
+
+      if (Friends.storage[message.username] !== undefined) {
+        $('.' + message.objectId)[0].parentElement.className = 'friend';
+      }
+
+      $('.' + message.objectId).siblings('.username').on('click', function() { 
+        Friends.toggleStatus(message.username); 
+        $('#message').val('');
+        App.fetch(App.stopSpinner);
+      });
     }
   }
+  // }
 
 };
