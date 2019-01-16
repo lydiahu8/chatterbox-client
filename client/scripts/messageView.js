b1f2e630147c8ce9df359b431008970dcb568cc0
lydiahu8 <lydiahu8@gmail.com>
2019-01-15 19:28:55 -0800

    Add timeAgo method to be user-friendly

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -4,9 +4,35 @@ var MessageView = {
       <div class="chat">
         <div class="username"><%- username %></div>
         <div class="text"><%- text %></div>
-        <div class="roomname"><%- roomname %></div>
+        <div class="createdAt"><% if(createdAt !== undefined) { %><%- MessageView.timeAgo(Date.now() - Date.parse(createdAt)) %> ago<% } %></div>
         <div style="display: none" class="<%- objectId %>"></div>
       </div>
-    `)
+    `),
+
+  timeAgo: function (date) {
+    var seconds = Math.floor(date / 1000);
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+      return `${interval} years`;
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return `${interval} months`;
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return `${interval} days`;
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return `${interval} hours`;
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return `${interval} minutes`;
+    }
+    return `${Math.floor(seconds)} seconds`;
+  }
 
 };
